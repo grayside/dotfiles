d24d6bc46c4c4eadfb57d1c1bdddef41cb63ce78
Grayside <grayside@gmail.com>
2016-04-03 22:49:59 -0700

    First pass import of Grayside vim configuration and initial vim-plug installtion sans plugins.

---
+++ vim/install.sh
@@ -0,0 +1,13 @@
+# Backups, swaps and undos are stored here.
+mkdir -p $DOTFILES/caches/vim
+
+if empty(glob('~/.vim/autoload/plug.vim'))
+  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall | source $MYVIMRC
+endif
+
+# Download Vim plugins.
+if [[ "$(type -P vim)" ]]; then
+  vim +PlugUpgrade +PlugUpdate +qall
+fi
