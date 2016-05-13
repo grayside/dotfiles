477d53c37a2cf90ad0242bc66e2686ce29b1074b
Grayside <aross@phase2technology.com>
2016-05-13 15:07:51 -0700

    Fix up the VIM configuration. Still a few bugs.

--- vim/install.sh
+++ vim/install.sh
@@ -1,12 +1,6 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $ZSH/caches/vim
 
-if empty(glob('~/.vim/autoload/plug.vim'))
-  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
-    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  autocmd VimEnter * PlugInstall | source $MYVIMRC
-endif
-
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
   vim +PlugUpgrade +PlugUpdate +qall
