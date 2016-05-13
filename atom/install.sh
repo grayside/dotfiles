e98df68492770588e6b4b4d90e11630cfe39500e
Grayside <aross@phase2technology.com>
2016-05-13 15:05:12 -0700

    Move around Atom and add some default packages.

---
+++ atom/install.sh
@@ -0,0 +1,17 @@
+# Git
+apm install git-log
+apm install git-time-machine
+apm install tree-view-git-branch
+
+# Minimap
+apm install minimap
+apm install minimap-cursorline
+apm install minimap-bookmarks
+apm install minimap-find-and-replace
+
+# General UX Enhancements
+apm install file-icons
+
+# PHP/Drupal
+apm install drupal
+apm install atom-drupal-api
