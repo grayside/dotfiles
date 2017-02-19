4b2a22ff3790b95ae24fce25b757e564213e8961
Grayside <aross@phase2technology.com>
2017-02-18 23:28:46 -0800

    Various minor changes.

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,3 @@
 alias fig='docker-compose $*'
 alias r="docker-compose -f build.yml run --rm $*"
+alias dte='eval "$(rig config)"'
--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,9 +1,25 @@
+
+#git
+#exec('git rev-parse --show-toplevel 2> /dev/null', $output);
+#if (!empty($output)) {
+#  $repo = $output[0];
+#  $options['config'] = $repo . '/drush/drushrc.php';
+#  $options['include'] = $repo . '/drush/commands';
+#  $options['alias-path'] = $repo . '/drush/aliases';
+#}
+
 dcon() {
   docker exec -it $1 bash
 }
 
+# Default to empty value for NCL docker-compose.yml
+export DOCKER_ENV=""
+
+# Configure Phase2 nameservers.
+export RIG_NAMESERVERS=”10.10.7.2:53,8.8.8.8:53”
+
 # Support for DevTools
-if test ! $(which devtools)
+if test $(which rig)
 then
-  eval "$(devtools config)"
+  eval "$(rig config)"
 fi
