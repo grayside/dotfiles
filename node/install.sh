27518ab1d6f6d0784b08b19c72e4a7590c95dda3
Adam Ross <aross@phase2technology.com>
2017-09-09 12:22:44 -0700

    Move nvm alias to install-only.

--- node/install.sh
+++ node/install.sh
@@ -12,3 +12,4 @@ export NVM_DIR="$HOME/.nvm"
 . "$(brew --prefix nvm)/nvm.sh"
 
 nvm install 4
+nvm alias default 4
--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,5 +1,3 @@
 export NVM_DIR="$HOME/.nvm"
 . "$(brew --prefix nvm)/nvm.sh"
 
-nvm alias default 4 > /dev/null
-
