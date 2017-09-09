9cd994568e0394b66cb0ad23bb80da8174da61aa
Adam Ross <aross@phase2technology.com>
2017-09-09 12:21:14 -0700

    Update docker aliases, inject gitconfigs with 'r'

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,3 @@
 alias fig='docker-compose $*'
-alias r="docker-compose -f build.yml run --rm $*"
-alias dte='eval "$(rig config)"'
+alias r="docker-compose -f build.yml -f ~/.outrigger.build.yml run --rm $*"
+alias rigenv='eval "$(rig config)"'
